0ff48a64cc72f717404a93eaaaeb33874d349038
Henry Dinhofer <hdinhof1@harvey.cc.binghamton.edu>
2015-04-30 18:20:22 -0400

    driver commented out

--- Driver.cpp
+++ Driver.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
-
+#include <ostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstring> 
+#include <stdlib.h>
 /*
 * Print the usage string
 */
@@ -8,5 +13,29 @@ void usageString() {
 }
 
 int main(int argc, const char *argv[]){
-	//TODO: Main..
+	clock_t begin = clock();
+    /*argv[1]: <input file name>
+    argv[2]: <output file name>
+    argv[3]: 1(linear), 2(quadratic), 3(double), 4(perfect)
+    */
+	
+	/*
+	string line, input; //yeah ill use strings. 
+	std::ifstream in (argv[1] ); // input
+	std::ofstream outf (argv[2], ios::app );
+	if (!outf.is_open() || !in.is_open() || argc != 3)  {std::cout << "error opening input /output files please check again " << argv[1] << " " << argv[2] << std::endl;  }
+	string input;
+if ( prices.good() ) {
+        while ( getline (prices, input) ) {
+		// TODO	
+
+	}
+}
+	prices.close();
+    clock_t end = clock();
+    double t_elapsed = double (end - begin) / (CLOCKS_PER_SEC);
+
+// outf is outputfile
+
+	*/
 }
